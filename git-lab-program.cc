aa9441028fee9cb08bee2d4aaaf2fea47cfde852
Owen Lyons <owen.lyons3434@gmail.com>
2024-01-26 15:31:19 -0500

    Hi!

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Owen Lyons>
+ *        Date: <January 26, 2024>
+ * Description: This is a description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git" << endl;
+    return 0;
+}// main
+
